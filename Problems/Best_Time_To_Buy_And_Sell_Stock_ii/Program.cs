92c5cd22842bf2326d26cd6a6e43c209c06f15e8
htlsmile <htlsmile@vip.qq.com>
2018-04-16 20:26:29 +0800

    2018-4-16 20:26:21

---
+++ Problems/Best_Time_To_Buy_And_Sell_Stock_ii/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Best_Time_To_Buy_And_Sell_Stock_ii
+{
+    class Program
+    {
+        static Solution solution = new Solution();
+
+        static void Main(string[] args)
+        {
+            int[] prices = { 1, 7, 4, 3, 1, 5 };
+            var maxProfit = solution.MaxProfit(prices);
+            Console.WriteLine(maxProfit);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Problems/Best_Time_To_Buy_And_Sell_Stock_ii/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Best_Time_To_Buy_And_Sell_Stock_ii
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int profit = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                if (prices[i] < prices[i + 1])
+                {
+                    profit += (prices[i + 1] - prices[i]);
+                }
+            }
+            return profit;
+        }
+    }
+}
