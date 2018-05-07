ddb0807553278f69255d4f5472afa0464cf3e82f
htlsmile <htlsmile@vip.qq.com>
2018-05-07 21:09:13 +0800

    2018-5-7 21:09:09

---
+++ Problems/_3Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _3Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -1, 0, 1, 2, -1, -4 };
+            Test(nums);
+            Console.ReadKey(true);
+        }
+
+        static void Test(int[] nums)
+        {
+            var solution = new Solution();
+            var result = solution.ThreeSum(nums);
+            for (int i = 0; i < result.Count; i++)
+            {
+                for (int j = 0; j < result[i].Count; j++)
+                {
+                    Console.Write($"{result[i][j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Problems/_3Sum/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3Sum
+{
+    public class Solution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums);
+            List<IList<int>> result = new List<IList<int>>();
+            int last = int.MaxValue;
+            for (int i = 0; i < nums.Length - 2 && nums[i] <= 0; i++)
+            {
+                if (nums[i] == last)
+                {
+                    continue;
+                }
+                last = nums[i];
+                int j = i + 1;
+                int k = nums.Length - 1;
+                while (j < k)
+                {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum < 0)
+                    {
+                        j++;
+                    }
+                    else if (sum == 0)
+                    {
+                        var temp = new List<int>
+                            {
+                                nums[i],
+                                nums[j],
+                                nums[k]
+                            };
+                        result.Add(temp);
+                        while (j < k && nums[j] == nums[j + 1])
+                        {
+                            j++;
+                        }
+                        while (j < k && nums[k] == nums[k - 1])
+                        {
+                            k--;
+                        }
+                        j++;
+                        k--;
+                    }
+                    else if (sum > 0)
+                    {
+                        k--;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
