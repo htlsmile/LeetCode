84a60e718fafae16f824c95e34542b3d2a1039d3
htlsmile <htlsmile@vip.qq.com>
2018-05-08 20:13:03 +0800

    2018-5-8 20:12:59

---
+++ Problems/Set_Matrix_Zeroes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Set_Matrix_Zeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] m1 = { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
+            Test(m1);
+            int[,] m2 = { { 0, 1, 2, 0 }, { 3, 4, 5, 2 }, { 1, 3, 1, 5 } };
+            Test(m2);
+            Console.ReadKey(true);
+        }
+
+        static void Test(int[,] matrix)
+        {
+            var solution = new Solution();
+            solution.SetZeroes(matrix);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Problems/Set_Matrix_Zeroes/Solution.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Set_Matrix_Zeroes
+{
+    public class Solution
+    {
+        public void SetZeroes(int[,] matrix)
+        {
+            bool f = false;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (!f && matrix[i, 0] == 0)
+                {
+                    f = true;
+                }
+                for (int j = 1; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        matrix[i, 0] = matrix[0, j] = 0;
+                    }
+                }
+            }
+            for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
+            {
+                for (int j = matrix.GetLength(1) - 1; j > 0; j--)
+                {
+                    if (matrix[i, 0] == 0 || matrix[0, j] == 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+                if (f)
+                {
+                    matrix[i, 0] = 0;
+                }
+            }
+        }
+
+        public void SetZeroes_1(int[,] matrix)
+        {
+            var rows = new List<int>();
+            var cols = new List<int>();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        if (!rows.Contains(i))
+                        {
+                            rows.Add(i);
+                        }
+                        if (!cols.Contains(j))
+                        {
+                            cols.Add(j);
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (rows.Contains(i) || cols.Contains(j))
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+
+        }
+
+        public void SetZeroes_2(int[,] matrix)
+        {
+            var matrix2 = matrix.Clone() as int[,];
+            for (int i = 0; i < matrix2.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix2.GetLength(1); j++)
+                {
+                    if (matrix2[i, j] == 0)
+                    {
+                        for (int k = 0; k < matrix.GetLength(0); k++)
+                        {
+                            matrix[k, j] = 0;
+                        }
+                        for (int k = 0; k < matrix.GetLength(1); k++)
+                        {
+                            matrix[i, k] = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
