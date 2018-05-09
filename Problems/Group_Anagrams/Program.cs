7f1a209cf6ee3c096602043b32a94c68a72b5b5e
htlsmile <htlsmile@vip.qq.com>
2018-05-09 19:23:22 +0800

    2018-5-9 19:23:19

---
+++ Problems/Group_Anagrams/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Group_Anagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+            Test(strs);
+            Console.ReadKey(true);
+        }
+
+        static void Test(string[] strs)
+        {
+            var solution = new Solution();
+            var result = solution.GroupAnagrams(strs);
+            foreach (var strList in result)
+            {
+                foreach (var str in strList)
+                {
+                    Console.Write($"{str}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Problems/Group_Anagrams/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group_Anagrams
+{
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dict = new Dictionary<string, List<string>>();
+            for (int i = 0; i < strs.Length; i++)
+            {
+                var charArray = strs[i].ToCharArray();
+                Array.Sort(charArray);
+                var key = new string(charArray);
+                if (!dict.ContainsKey(key))
+                {
+                    dict.Add(key, new List<string>() { strs[i] });
+                }
+                else
+                {
+                    dict[key].Add(strs[i]);
+                }
+            }
+            var result = new List<IList<string>>(dict.Values);
+            return result;
+        }
+    }
+}
