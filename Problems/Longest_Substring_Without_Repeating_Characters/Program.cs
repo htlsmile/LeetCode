e3636bce8ee3e861e9c1e67d644812518b503913
htlsmile <htlsmile@vip.qq.com>
2018-05-10 22:11:55 +0800

    2018-5-10 22:11:52

---
+++ Problems/Longest_Substring_Without_Repeating_Characters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Longest_Substring_Without_Repeating_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test("abcabcbb");
+            Test("bbbbb");
+            Test("pwwkew");
+            Test("c");
+            Test("dvdf");
+            Test("abcbad");
+            Console.ReadKey(true);
+        }
+
+        static void Test(string s)
+        {
+            var solution = new Solution();
+            var result = solution.LengthOfLongestSubstring(s);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Problems/Longest_Substring_Without_Repeating_Characters/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Longest_Substring_Without_Repeating_Characters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            List<char> list = new List<char>();
+            int maxLength = 0, i = 0, j = 0;
+            while (i < s.Length && j < s.Length)
+            {
+                if (!list.Contains(s[j]))
+                {
+                    list.Add(s[j]);
+                    j++;
+                    maxLength = Math.Max(maxLength, j - i);
+                }
+                else
+                {
+                    list.Remove(s[i]);
+                    i++;
+                }
+            }
+            return maxLength;
+        }
+    }
+}
