e59111ef9be7949ea1a438f9ebd5fd730717cc49
htlsmile <htlsmile@vip.qq.com>
2018-05-16 20:47:29 +0800

    2018-5-16 20:47:26

---
+++ Problems/Common/BinaryTree.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Common
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class BinaryTree
+    {
+        public static void PreorderTraversal(TreeNode node, Action<int> action = null)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            if (action == null)
+            {
+                action = val => Console.Write($"{val} ");
+            }
+            action(node.val);
+            PreorderTraversal(node.left, action);
+            PreorderTraversal(node.right, action);
+        }
+
+        public static void InorderTraversal(TreeNode node, Action<int> action = null)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            if (action == null)
+            {
+                action = val => Console.Write($"{val} ");
+            }
+            InorderTraversal(node.left, action);
+            action(node.val);
+            InorderTraversal(node.right, action);
+        }
+
+        public static void PostorderTraversal(TreeNode node, Action<int> action = null)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            if (action == null)
+            {
+                action = val => Console.Write($"{val} ");
+            }
+            PostorderTraversal(node.left, action);
+            PostorderTraversal(node.right, action);
+            action(node.val);
+        }
+
+        public static TreeNode GetTestTree()
+        {
+            var tree = new TreeNode(3)
+            {
+                left = new TreeNode(9),
+                right = new TreeNode(20)
+                {
+                    left = new TreeNode(15),
+                    right = new TreeNode(7)
+                }
+            };
+            return tree;
+        }
+    }
+}
---
+++ Problems/Construct_Binary_Tree_From_Preorder_And_Inorder_Traversal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Construct_Binary_Tree_From_Preorder_And_Inorder_Traversal
+{
+    using Common;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] preorder = { 3, 9, 20, 15, 7 };
+            int[] inorder = { 9, 3, 15, 20, 7 };
+            Test(preorder, inorder);
+            Console.ReadKey(true);
+        }
+
+        static void Test(int[] preorder, int[] inorder)
+        {
+            var solution = new Solution();
+            var tree = solution.BuildTree(preorder, inorder);
+            BinaryTree.PreorderTraversal(tree);
+            Console.WriteLine();
+            BinaryTree.InorderTraversal(tree);
+            Console.WriteLine();
+            BinaryTree.PostorderTraversal(tree);
+            Console.WriteLine();
+        }
+    }
+}
