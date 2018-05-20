b8ad172b71de80a23b69b70dca415f60255d7d2d
htlsmile <htlsmile@vip.qq.com>
2018-05-20 18:36:14 +0800

    2018-5-20 18:36:10

---
+++ Problems/Sort_Colors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sort_Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 0, 2, 1, 1, 0 };
+            Test(nums);
+            Console.ReadKey(true);
+        }
+
+        static void Test(int[] nums)
+        {
+            var solution = new Solution();
+            solution.SortColors(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Problems/Sort_Colors/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sort_Colors
+{
+    public class Solution
+    {
+        public void SortColors(int[] nums)
+        {
+            int[] index = { -1, -1, -1 };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = nums[i]; j > -1; j--)
+                {
+                    if (index[j] != -1)
+                    {
+                        index[nums[i]] = index[j] + 1;
+                        break;
+                    }
+                }
+                if (index[nums[i]] == -1)
+                {
+                    index[nums[i]]++;
+                }
+                for (int j = nums[i] + 1; j < index.Length; j++)
+                {
+                    if (index[j] != -1)
+                    {
+                        index[j]++;
+                    }
+                }
+                int cur = index[nums[i]];
+                if (i > cur)
+                {
+                    int t = nums[i];
+                    for (int j = i; j > cur; j--)
+                    {
+                        nums[j] = nums[j - 1];
+                    }
+                    nums[cur] = t;
+                }
+            }
+        }
+    }
+}
