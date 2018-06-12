81dffd876aa9da20e3c2ae20a91df6160d645d97
htlsmile <htlsmile@vip.qq.com>
2018-06-12 20:31:32 +0800

    2018-6-12 20:31:29

---
+++ Problems/Insert_Delete_Getrandom_O1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Insert_Delete_GetRandom_O1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+            Console.ReadKey(true);
+        }
+
+        static void Test()
+        {
+            RandomizedSet set = new RandomizedSet();
+            Console.WriteLine(set.Insert(3));
+            Console.WriteLine(set.Insert(-2));
+            Console.WriteLine(set.Remove(2));
+            Console.WriteLine(set.Insert(1));
+            Console.WriteLine(set.Insert(-3));
+            Console.WriteLine(set.Insert(-2));
+            Console.WriteLine(set.Remove(-2));
+            Console.WriteLine(set.Remove(3));
+            Console.WriteLine(set.Insert(-1));
+            Console.WriteLine(set.Remove(-3));
+            Console.WriteLine(set.Insert(1));
+            Console.WriteLine(set.Insert(-2));
+            Console.WriteLine(set.Insert(-2));
+            Console.WriteLine(set.Insert(-2));
+            Console.WriteLine(set.Insert(1));
+            Console.WriteLine(set.GetRandom());
+            Console.WriteLine(set.Insert(-2));
+            Console.WriteLine(set.Remove(0));
+            Console.WriteLine(set.Insert(-3));
+            Console.WriteLine(set.Insert(1));
+
+        }
+    }
+}
---
+++ Problems/Insert_Delete_Getrandom_O1/RandomizedSet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insert_Delete_GetRandom_O1
+{
+    public class RandomizedSet
+    {
+        private readonly Random rand;
+        private readonly List<int> list;
+        private readonly Dictionary<int, int> dict;
+
+        public RandomizedSet()
+        {
+            rand = new Random();
+            list = new List<int>();
+            dict = new Dictionary<int, int>();
+        }
+
+        public bool Insert(int val)
+        {
+            if (dict.ContainsKey(val))
+            {
+                return false;
+            }
+            dict.Add(val, list.Count);
+            list.Add(val);
+            return true;
+        }
+
+        public bool Remove(int val)
+        {
+            if (!dict.ContainsKey(val))
+            {
+                return false;
+            }
+            int index = dict[val];
+            if (index < list.Count - 1)
+            {
+                int last = list[list.Count - 1];
+                list[index] = last;
+                dict[last] = index;
+            }
+            dict.Remove(val);
+            list.RemoveAt(list.Count - 1);
+            return true;
+        }
+
+        public int GetRandom()
+        {
+            return list.Count == 0 ? 0 : list[rand.Next(list.Count)];
+        }
+    }
+}
