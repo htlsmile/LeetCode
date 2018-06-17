7cb2bacf5fd4eee048df5a03c2012d8e8652529f
htlsmile <htlsmile@vip.qq.com>
2018-06-17 15:44:35 +0800

    2018-6-17 15:44:27

---
+++ Problems/First_Missing_Positive/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace First_Missing_Positive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 2, 0 };
+            Test(nums1);
+            int[] nums2 = { 3, 4, -1, 1 };
+            Test(nums2);
+            int[] nums3 = { 7, 8, 9, 11, 12 };
+            Test(nums3);
+            int[] nums4 = { 10, 9, 8, 7, 6, 4, 3, 2, 1, 0, -1, -2, -3 };
+            Test(nums4);
+            int[] nums5 = { 1, 2, 3, 4, 5 };
+            Test(nums5);
+            int[] nums6 = { 1,-1,-2,-3 };
+            Test(nums6);
+            Console.ReadKey(true);
+        }
+
+        static void Test(int[] nums)
+        {
+            var solution = new Solution();
+            var result = solution.FirstMissingPositive(nums);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Problems/First_Missing_Positive/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace First_Missing_Positive
+{
+    public class Solution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (nums[i] > 0 && nums[i] <= nums.Length && nums[nums[i] - 1] != nums[i])
+                {
+                    int t = nums[nums[i] - 1];
+                    nums[nums[i] - 1] = nums[i];
+                    nums[i] = t;
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i + 1)
+                {
+                    return i + 1;
+                }
+            }
+            if (nums.Length > 0 && nums[nums.Length - 1] == nums.Length)
+            {
+                return nums.Length + 1;
+            }
+            return 1;
+        }
+    }
+}
