5f23c95bb5cd84a40b977c2df31728bb78012c7e
htlsmile <htlsmile@vip.qq.com>
2018-06-20 20:49:36 +0800

    2018-6-20 20:49:33

---
+++ Problems/Basic_Calculator_ii/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Basic_Calculator_ii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test("3+2*2");//7
+            Test(" 3/2 ");//1
+            Test(" 3+5 / 2 ");//5
+            Test(" 1 + 2 - 3 * 4 / 5 * 6 + 7 ");//-2
+            Test(" 12 - 34 * 56 + 78 / 9 ");//-1884
+            Console.ReadKey(true);
+        }
+
+        static void Test(string s)
+        {
+            var solution = new Solution();
+            var result = solution.Calculate(s);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Problems/Basic_Calculator_ii/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Calculator_ii
+{
+    public class Solution
+    {
+        public int Calculate(string s)
+        {
+            s = s.Replace(" ", "");
+            Stack<int> nums = new Stack<int>();
+            int n = 0;
+            char sign = '+';
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsDigit(s[i]))
+                {
+                    n = n * 10 + s[i] - '0';
+                }
+                if (!char.IsDigit(s[i]) || i == s.Length - 1)
+                {
+                    if (sign == '+')
+                    {
+                        nums.Push(n);
+                    }
+                    else if (sign == '-')
+                    {
+                        nums.Push(-n);
+                    }
+                    else if (sign == '*')
+                    {
+                        nums.Push(nums.Pop() * n);
+                    }
+                    else if (sign == '/')
+                    {
+                        nums.Push(nums.Pop() / n);
+                    }
+                    sign = s[i];
+                    n = 0;
+                }
+            }
+            int result = 0;
+            while (nums.Count > 0)
+            {
+                result += nums.Pop();
+            }
+            return result;
+        }
+    }
+}
